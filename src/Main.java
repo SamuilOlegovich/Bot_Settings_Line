081fab6d656391a8fcfad6abc5f56d78e6ef9e83
SamuilOlegovich <47562089+SamuilOlegovich@users.noreply.github.com>
2020-09-20 15:20:47 +0300

    save data

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+public class Main {
+    public static void main(String[] args) {
+
+        StringBuilder stringBuilder = new StringBuilder("AdditionalTrading1 = [");
+        double value = 0.001;
+        double steep = 0.1;
+        double plus = 0.10;
+        double take = 0.6;
+        int block = 100;
+
+        for (int i = 0; i < block; i++) {
+            String[] strings = ("" + steep).split("\\.");
+            String string = strings[0] + "." + strings[1].substring(0, strings[1].length() == 1 ? 1 : 2);
+
+            stringBuilder.append("\"").append(string)
+                    .append("@P*").append(take)
+                    .append("@P*").append(value)
+                    .append("@V\"");
+            if (i != block - 1) {
+                stringBuilder.append(", ");
+            } else {
+                stringBuilder.append("]");
+            }
+            steep = Double.parseDouble(string) + plus;
+
+        }
+        System.out.println(stringBuilder.toString());
+
+    }
+}
