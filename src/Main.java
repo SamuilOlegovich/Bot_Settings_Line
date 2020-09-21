db5aefd46e2f7104cc5b4594f4dca8b55c726ed5
SamuilOlegovich <47562089+SamuilOlegovich@users.noreply.github.com>
2020-09-21 14:42:23 +0300

    add

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,72 @@
 public class Main {
-    public static void main(String[] args) {
 
+
+    public static void main(String[] args) {
+//       stringSteepMartinTwo();
+       stringSteepMartin();
+    }
+
+
+
+    // BalanceRestBuy = 20*40*60*120*240*480*960*2560*5120*10240@V
+    // BalanceRestSell = 0,0001@V
+    // Interval = oneMin
+    // FreqUpdate = 2000
+    // SellOnlyBought = False
+    // WriteToFile = True
+    // IsDirectionLong = True
+    // StepRepresentEnter = 4*4,17*4,35*4,54*7,14*7,69*10,41*11,63*15,79@P
+    // StepRepresentClose = 1,7*1,7*1,7*1,9*1,9*2*2*2,2*2,2*2,2*2,2@P
+    private static void stringSteepMartin() {
+        double value = 0.001;
+        double steep = 0.1;
+        double take = 0.6;
+        int cicle = 100;
+
+        StringBuilder stringBuilder = new StringBuilder("BalanceRestBuy = 0.0001@V\nBalanceRestSell = ");
+
+        for (int i = 0; i < cicle; i++) {
+            stringBuilder.append(value);
+            if (i < cicle - 1) {
+                stringBuilder.append("*");
+            } else {
+                stringBuilder.append("@V");
+            }
+            value = value + 0.001;
+        }
+
+        stringBuilder.append("\nInterval = oneMin\n" +
+                "FreqUpdate = 2000\n" +
+                "SellOnlyBought = False\n" +
+                "WriteToFile = True\n" +
+                "IsDirectionLong = True\n" +
+                "StepRepresentEnter = ");
+
+        for (int i = 0; i < cicle; i++) {
+            stringBuilder.append("0.3");
+            if (i < cicle - 1) {
+                stringBuilder.append("*");
+            } else {
+                stringBuilder.append("@P");
+            }
+        }
+
+        stringBuilder.append("\nStepRepresentClose = ");
+
+        for (int i = 0; i < cicle; i++) {
+            stringBuilder.append("0.8");
+            if (i < cicle - 1) {
+                stringBuilder.append("*");
+            } else {
+                stringBuilder.append("@P");
+            }
+        }
+        System.out.println(stringBuilder.toString());
+    }
+
+
+
+    private static void stringSteepMartinTwo() {
         StringBuilder stringBuilder = new StringBuilder("AdditionalTrading1 = [");
         double value = 0.001;
         double steep = 0.1;
@@ -22,9 +88,7 @@ public class Main {
                 stringBuilder.append("]");
             }
             steep = Double.parseDouble(string) + plus;
-
         }
         System.out.println(stringBuilder.toString());
-
     }
 }
